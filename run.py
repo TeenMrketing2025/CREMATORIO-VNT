89b9a4aff0b0f5cfec1fcbdf86b3812c18ca365a
TeenMrketing2025 <paginas.marketing@grouptorreblanca.com>
2025-07-08 08:28:52 -0500

    Second commit

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 import logging
 import os
+from flask import Flask, redirect
+
 
 from app import create_app
 
 
 app = create_app()
+
+@app.route("/")
+def home():
+    return redirect("https://crematoriovirgendelanatividadtpp.com/")
 
 if __name__ == "__main__":
     logging.info("Flask app started")
